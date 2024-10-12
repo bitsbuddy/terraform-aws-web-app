e9a45580ac3647baaf4641b0afd5b45df61991d2
bitsbuddy <48157073+bitsbuddy@users.noreply.github.com>
2024-10-12 16:31:01 -0400

    Update from AI assistant

--- s3.tf
+++ s3.tf
@@ -1,18 +1,13 @@
-# Define S3 resources.
-# https://aws.amazon.com/s3
 
-# https://www.terraform.io/docs/providers/aws/r/s3_bucket.html
-resource "aws_s3_bucket" "domain" {
+resource "aws_s3_bucket" "new_bucket" {
   # The name of the bucket.
-  bucket = local.bucket_name
+  bucket = "new-bucket-name"
 
   # The canned ACL to apply. Defaults to "private".
-  #   https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl
-  acl = "public-read"
+  acl = "private"
 
   # Configure your bucket as a static website. It'll be available
   #   at the AWS Region-specific website endpoint of the bucket.
-  #   http://bucket-name.s3-website-Region.amazonaws.com
   website {
     index_document = "index.html"
   }
@@ -32,34 +27,12 @@ resource "aws_s3_bucket" "domain" {
     enabled = true
   }
 
-  # Define how Amazon S3 manages objects during their lifetime.
-  lifecycle_rule {
-    # Object key prefix identifying one or more objects to apply the rule.
-    prefix = "/"
-
-    # Enable this lifecycle rule.
-    enabled = true
-
-    noncurrent_version_transition {
-      # Specify the number of days after object creation
-      #   when the specific rule action takes effect.
-      days = 30
-
-      # Specifies the Amazon S3 storage class.
-      storage_class = "DEEP_ARCHIVE"
-    }
-
-    noncurrent_version_expiration {
-      days = 365
-    }
-  }
-
   # All objects (including locked) are deleted when deleting a bucket.
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "domain_policy" {
-  bucket = aws_s3_bucket.domain.id
+resource "aws_s3_bucket_policy" "new_bucket_policy" {
+  bucket = aws_s3_bucket.new_bucket.id
 
   policy = <<POLICY
 {
@@ -73,7 +46,7 @@ resource "aws_s3_bucket_policy" "domain_policy" {
                 "s3:GetObject"
             ],
             "Resource": [
-                "arn:aws:s3:::${local.bucket_name}/*"
+                "arn:aws:s3:::new-bucket-name/*"
             ]
         }
     ]
@@ -81,14 +54,14 @@ resource "aws_s3_bucket_policy" "domain_policy" {
 POLICY
 }
 
-resource "aws_s3_bucket_object" "dist" {
-  for_each = fileset(var.dist_dir, "**")
+resource "aws_s3_bucket_object" "new_bucket_object" {
+  for_each = fileset("path/to/your/files", "**")
 
-  acl    = "public-read"
-  bucket = aws_s3_bucket.domain.id
+  acl    = "private"
+  bucket = aws_s3_bucket.new_bucket.id
   key    = each.value
-  source = "${var.dist_dir}/${each.value}"
-  etag   = filemd5("${var.dist_dir}/${each.value}")
+  source = "path/to/your/files/${each.value}"
+  etag   = filemd5("path/to/your/files/${each.value}")
 
   content_type = lookup(local.mime_types, split(".", each.value)[length(split(".", each.value)) - 1])
 }
