c9a4240f8b39082b6d0aacfb042653dbaa33a697
bitsbuddy <48157073+bitsbuddy@users.noreply.github.com>
2024-10-14 15:43:55 -0400

    Update from AI assistant

--- s3.tf
+++ s3.tf
@@ -192,3 +192,59 @@ resource "aws_s3_bucket_policy" "test1_policy" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "test3" {
+  # The name of the bucket.
+  bucket = "test3"
+
+  # The canned ACL to apply. Defaults to "private".
+  acl = "public-read"
+
+  # Configure your bucket as a static website. It'll be available
+  #   at the AWS Region-specific website endpoint of the bucket.
+  #   http://bucket-name.s3-website-Region.amazonaws.com
+  website {
+    index_document = "index.html"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # Enable versioning. Once you version-enable a bucket, it can never
+  #   return to an unversioned state. You can, however, suspend versioning
+  #   on that bucket.
+  versioning {
+    enabled = true
+  }
+
+  # All objects (including locked) are deleted when deleting a bucket.
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "test3_policy" {
+  bucket = aws_s3_bucket.test3.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::test3/*"
+            ]
+        }
+    ]
+}
+POLICY
+}
