a3d374f3d929a68a85148ae4bcc2b1a845b98816
bitsbuddy <48157073+bitsbuddy@users.noreply.github.com>
2024-10-15 15:31:42 -0400

    Update from AI assistant

--- s3.tf
+++ s3.tf
@@ -202,11 +202,52 @@ resource "aws_s3_bucket_policy" "new_bucket_3_policy" {
 POLICY
 }
 
-resource "aws_s3_bucket_object" "new_bucket_3_object" {
+resource "aws_s3_bucket" "new_bucket_4" {
+  # The name of the bucket.
+  bucket = "new-bucket-4-name"
+
+  # The canned ACL to apply. Defaults to "private".
+  acl = "private"
+
+  # Enable versioning. Once you version-enable a bucket, it can never
+  #   return to an unversioned state. You can, however, suspend versioning
+  #   on that bucket.
+  versioning {
+    enabled = true
+  }
+
+  # All objects (including locked) are deleted when deleting a bucket.
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "new_bucket_4_policy" {
+  bucket = aws_s3_bucket.new_bucket_4.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::new-bucket-4-name/*"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_object" "new_bucket_4_object" {
   for_each = fileset(var.dist_dir, "**")
 
   acl    = "private"
-  bucket = aws_s3_bucket.new_bucket_3.id
+  bucket = aws_s3_bucket.new_bucket_4.id
   key    = each.value
   source = "${var.dist_dir}/${each.value}"
   etag   = filemd5("${var.dist_dir}/${each.value}")
